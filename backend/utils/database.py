477f61d49291673ba28a520a180437ad95c9652f
Muthommi <kelvinmartin16@gmail.com>
2025-09-23 10:56:44 +0300

    correcting my original app

--- backend/utils/database.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-
-from pymongo import MongoClient
-
-client = MongoClient('mongodb://localhost:27017/')
-db = client['finance_tracker']
-
-users_collection = db['users']
-transactions_collection = db['transactions']
